515a5fa42c521d43d941181b01a9c8828f9e4c2f
Jeremy Prevost <JPrevost@users.noreply.github.com>
2021-08-30 10:42:13 -0400

    SQS input queue reader
    Why are these changes being introduced:
    
    * this application needs to read from the SQS input queue
    * this application needs to write to the output queue
    
    Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/ETD-400
    
    How does this address that need:
    
    * sets up a `start` method in the application that will request and
      process messages from the SQS input queue until no messages are returned
    * messages are removed from the SQS input queue when they are processed
    * the processing is fake for now, but is stubbed out to allow the dspace
      submission to be added in future commits
    * results are added to the SQS output queue. As we don't actually submit
      to dspace at this time, all output messages are "success" messages
    * a set of sample data is easily loadable to test the ability to read
      and write. In the future these can be expanded or modified to be
      useful in put testing and QA.
    
    Document any side effects or limitations to this change:
    
    - there is no actual processing of records
    - queue names are hard coded into the application. These will need to
      change to configuration in the future. Very likely using env locally
      and an AWS secrets management solution for staging and produciton.
    - there are no tests

--- submitter/cli.py
+++ submitter/cli.py
@@ -1,10 +1,96 @@
+import boto3
 import click
 
-
-@click.command()
-def hello():
-    click.echo("Hello World!")
+from submitter.sample_data import sample_data
 
 
-if __name__ == "__main__":
-    hello()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def start():
+    click.echo("Processing starting")
+    message_loop()
+
+
+def message_loop():
+    msgs = retrieve()
+
+    if len(msgs) > 0:
+        click.echo(len(msgs))
+        process(msgs)
+        message_loop()
+    else:
+        click.echo("No messages received")
+
+
+def retrieve():
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName="queue1-stage")
+
+    click.echo("Polling for messages")
+    msgs = queue.receive_messages(
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=20,
+        MessageAttributeNames=["All"],
+        AttributeNames=["All"],
+    )
+
+    return msgs
+
+
+def write(status):
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName="queue2-stage")
+
+    # Send message to SQS result queue
+    queue.send_message(
+        MessageAttributes=status,
+        MessageBody=("testing"),
+    )
+
+
+def process(msgs):
+    for message in msgs:
+        click.echo(message.message_attributes)
+        click.echo(message.body)
+        print("Do all the dspace submission stuff here")
+
+        # faking it with always succeeding for now... creating of this status
+        # dict is likely better moved to our upcoming submission class but this
+        # was convenient for initial testing
+        status = {
+            "PackageSource": {
+                "DataType": "String",
+                "StringValue": message.message_attributes.get("PackageSource").get(
+                    "StringValue"
+                ),
+            },
+            "PackageID": {
+                "DataType": "String",
+                "StringValue": message.message_attributes.get("PackageID").get(
+                    "StringValue"
+                ),
+            },
+            "status": {"DataType": "String", "StringValue": "success"},
+            "handle": {
+                "DataType": "String",
+                "StringValue": "https://example.com/handle/this",
+            },
+        }
+
+        # write result to output
+        write(status)
+
+        # cleanup (probs better to confirm the write to the output was good
+        # before cleanup but for now yolo it)
+        message.delete()
+
+
+@main.command()
+def sample_data_loader():
+    click.echo("sample this!")
+    sample_data()
+    click.echo("sample data (probably) loaded into input queue")
---
+++ submitter/sample_data.py
@@ -0,0 +1,117 @@
+import json
+
+import boto3
+
+
+def sample_data_loader(id, source, target, col_hdl, meta_loc, filename, fileloc):
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName="queue1-stage")
+    body = {
+        "SubmissionSystem": target,
+        "CollectionHandle": col_hdl,
+        "MetadataLocation": meta_loc,
+        "Files": [
+            {"BitstreamName": filename, "FileLocation": fileloc},
+        ],
+    }
+    # Send message to SQS queue
+    queue.send_message(
+        MessageAttributes={
+            "PackageID": {"DataType": "String", "StringValue": id},
+            "PackageSource": {"DataType": "String", "StringValue": source},
+        },
+        MessageBody=(json.dumps(body)),
+    )
+
+
+def sample_data():
+    sample_data_loader(
+        "123",
+        "ETD",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "466",
+        "ETD",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "789",
+        "ETD",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "asd",
+        "Wiley",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "feg",
+        "Wiley",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "hij",
+        "Wiley",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "etd_123",
+        "ETD",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "wiley_456",
+        "Wiley",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "orange",
+        "ETD",
+        "DSpace@MIT",
+        "1721.1/131022",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
+    sample_data_loader(
+        "cat",
+        "popcorn",
+        "devnull",
+        "whatever",
+        "s3:/fakeloc/a.json",
+        "file 1",
+        "s3:/fakeloc2/f.json",
+    )
